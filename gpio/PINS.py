3d15cb6a79e9602765ab336b318d82cdf2a504ad
Joel Grayson <joel@joelgrayson.com>
2023-01-27 19:57:32 -0500

    Fix pins

--- gpio/PINS.py
+++ gpio/PINS.py
@@ -1,14 +1,14 @@
 PINS={ #when changing here, change executable pins as well
-    "button-led": { #indicates if pi has power
-        "power":  {'pin': 18},
-        "ground": {'pin': 6}
+    'button': { #see if button pressed
+        'output': { 'pin': 8,  'gpio': 14 },
+        'input':  { 'pin': 10, 'gpio': 15 }
     },
-    "button": { #see if button pressed
-        "output": {'pin': 8,  "gpio": 14},
-        "input":  {'pin': 10, "gpio": 15}
+    'button-led': { #indicates if pi has power
+        'ground': { 'pin': 6 },
+        'power':  { 'pin': 12, 'gpio': 18 }
     },
-    "monitor": { #turn monitor on/off through relay
-        "ground": {'pin': 14},
-        "output": {'pin': 16, "gpio": 23}
+    'monitor': { #turn monitor on/off through relay
+        'ground': { 'pin': 20 },
+        'output': { 'pin': 22, 'gpio': 25 }
     }
 }
--- gpio/schematic.md
+++ gpio/schematic.md
@@ -2,12 +2,16 @@
 The monitor is plugged into the "normally on" outlet of the relay, so that it is always on even while the pi's GPIO pins aren't on yet. This means sending a HIGH signal, turns off the monitor and a LOW signal keeps the monitor on.
 
 # Schematic
+## Button
 * pin 6 - ground for the button's LED (white wire)
 * pin 8 - GPIO14 for outputing electricity (blue wire)
 * pin 10 - GPIO15 for reading electricity to see if button pressed (red wire)
     if read_input()==1, button is pressed (closed circuit)
     if read_input()==0, button is not pressed (open circuit)
 * pin 12 - GPIO18 as power to the button's LED (orange wire)
+## Monitor
+* pin 20 - ground for the monitor
+* pin 22 - GPIO25 for outputing electricity to the monitor
 
 
 Wiring Diagram:
