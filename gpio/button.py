72581656884202c85a902b6799972b1e7399ff85
Joel Grayson <joel@joelgrayson.com>
2023-01-28 09:39:07 -0500

    turn off after holding down for three seconds

--- gpio/button.py
+++ gpio/button.py
@@ -14,7 +14,8 @@ def listen():
     btn=InputDevice(PINS['button']['output']['gpio']) #read if button pressed
 
     last_pressed=None #datetime of last button press
-    will_turn_off_at=None
+    will_turn_off_at=None #for the kiosk to turn itself off after 10 minutes if not supposed to be on according to the schedule
+    turning_off_active_started=None #time when the button was first activated to turn off the monitor (hold down button for 3 seconds to turn off the monitor)
     
     while True: #Listen for button press
         curr_status=status() #current monitor status
@@ -27,6 +28,13 @@ def listen():
                 turn_off()
                 will_turn_off_at=None
 
+        if turning_off_active_started!=None and time()>turning_off_active_started: #turn off after holding down for three seconds
+            if curr_status=="on":
+                turn_off()
+                print('Turned monitor off')
+            turning_off_active_started=None
+            last_pressed=time()
+
         if btn.is_active: #button pressed ∵ btn-o15 circuit connected
             if last_pressed!=None and last_pressed+1>time(): continue #ignore if btn pressed less than a second ago
             
@@ -35,12 +43,13 @@ def listen():
             if curr_status=="off":
                 turn_on()
                 print('Turned monitor on')
+                turning_off_active_started=None
                 if not should_be_on_now():
                     print('Will turn off in 10 minutes because off-hour')
                     will_turn_off_at=time()+10*60 #turn off after 10 minutes of inactivity during after-hours
-            elif curr_status=="on": #already on, so no difference
-                turn_off()
-                print('Turned monitor off')
+            elif curr_status=="on":
+                if turning_off_active_started==None: #trigger turn off after 3 seconds if still holding
+                    turning_off_active_started=time()+3 #3 seconds later
             else:
                 print('Unknown curr_status:', curr_status)
 
